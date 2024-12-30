e3be5060211e3e57888cdfbbcc3a9674d7a32183
Adrian Kane <egg_on_stilts@hotmail.com>
2024-12-30 23:06:17 +1100

    Added a arm, crappy control code for everything enabled over WiFi

---
+++ include/arm.hpp
@@ -0,0 +1,61 @@
+// https://youtu.be/uYnBKLXCG6A?t=9
+/* Pin Definitions */
+#include <Arduino.h>
+#include <ESP32Servo.h> // Install Library
+
+// Servo signals
+#define SERVO_A_SIGNAL 12
+#define SERVO_B_SIGNAL 13
+#define SERVO_C_SIGNAL 14
+
+#define ANGLE_DELTA 5
+
+#define A_MOVE_POS 't'
+#define A_MOVE_NEG 'g'
+#define B_MOVE_POS 'y'
+#define B_MOVE_NEG 'h'
+#define C_MOVE_POS 'u'
+#define C_MOVE_NEG 'j'
+
+Servo ServeArm[3];
+
+void init_arm() {
+    ServeArm[0].attach(SERVO_A_SIGNAL);
+    ServeArm[1].attach(SERVO_B_SIGNAL);
+    ServeArm[2].attach(SERVO_C_SIGNAL);
+    ServeArm[0].write(0);
+    ServeArm[1].write(0);
+    ServeArm[2].write(0);
+}
+
+void arm_shift(Servo &p_servo, bool dir) {
+    int angle = p_servo.read();
+    if(dir){
+        angle = angle + ANGLE_DELTA;
+    } else {
+        angle = angle - ANGLE_DELTA;
+    }
+    p_servo.write(angle);
+}
+
+void arm_drive(char c){
+    if(A_MOVE_POS == c){
+        arm_shift(ServeArm[0], true);
+    }
+    if(A_MOVE_NEG == c){
+        arm_shift(ServeArm[0], false);
+    }
+    if(B_MOVE_POS == c){
+        arm_shift(ServeArm[1], true);
+    }
+    if(B_MOVE_NEG == c){
+        arm_shift(ServeArm[1], false);
+    }
+    if(C_MOVE_POS == c){
+        arm_shift(ServeArm[2], true);
+    }
+    if(C_MOVE_NEG == c){
+        arm_shift(ServeArm[2], false);
+    }
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "simpleWIFI.hpp"
 
+#include "arm.hpp"
+
 // Motor pin stubs - Replace with actual pin numbers
 /* 
  * https://github.com/Freenove/Freenove_Basic_Starter_Kit_for_ESP32_S3
@@ -24,6 +26,10 @@
 #define MOTOR_D_PWM 1  // Replace this
 #define MOTOR_D_DIR 7   // Replace this
 
+// M1A > M1B === FORWARD
+#define MOTOR_ARM_SPIN_DIFF_A 9  // M1A
+#define MOTOR_ARM_SPIN_DIFF_B 10 // M1B
+
 // Control keys
 #define LEFT 'a'
 #define RIGHT 'd'
@@ -32,9 +38,15 @@
 #define INFO 'i'
 #define HALT 'c'
 
+#define CLOCKWISE   'e'
+#define A_CLOCKWISE 'q'
+
 // 5 Speed Levels - 255/5 => 51
 #define MTRINC 51
 #define MTRDEC -51
+
+// Use Webserver
+#define WEB_INTERFACE
 
 // PWM max value
 #define PWMMAX 255
@@ -60,6 +72,12 @@ typedef struct mah_motor {
   uint8_t orientation;
 } MOTOR;
 
+typedef struct nah_motor {
+  int velocity;
+  uint8_t dir_A, pwm_B;
+  uint8_t orientation;
+} MOTOR_DIFF;
+
 // typedef struct mah_funcs {
 //   void (drive)(char);
 // } FUN_FUNC;
@@ -72,7 +90,12 @@ typedef struct mah_motor {
 void motor_init(MOTOR &p_motor, uint8_t direction_pin, uint8_t pwm_pin);
 int setSpeed(MOTOR &p_motor, int velocity);
 int motor_flip_orientation(MOTOR &p_motor);
+
+int setSpeed(MOTOR_DIFF &p_motor, int velocity);
+void motor_init(MOTOR_DIFF &p_motor, uint8_t direction_pin, uint8_t pwm_pin);
+
 void drive(char c);
+void drive_arm(char c);
 
 void MARS_WIFI_simple_simple_handle(){
     if (server.hasArg("plain")) {
@@ -82,6 +105,8 @@ void MARS_WIFI_simple_simple_handle(){
         int i = 0;
         while(str[i]!= 0){
           drive(str[i]);
+          drive_arm(str[i]);
+          arm_drive(str[i]);
           i++;
         }
 
@@ -99,6 +124,10 @@ MOTOR motor2;
 MOTOR motor3;
 MOTOR motor4;
 
+/* Arm stuff */
+MOTOR_DIFF motor_base;
+
+
 //##############################
 /* Main program entry point */
 
@@ -112,17 +141,27 @@ void setup() {
   motor_init(motor4, MOTOR_D_DIR, MOTOR_D_PWM);
   motor_flip_orientation(motor2);
 
+  motor_init(motor_base, MOTOR_ARM_SPIN_DIFF_A, MOTOR_ARM_SPIN_DIFF_B);
+
   Serial.println("HELLO TERMINAL");
+#ifdef WEB_INTERFACE
   MARS_WIFI_init(MARS_WIFI_simple_simple_handle);
+#endif
+
+  init_arm();
 }
 
 void loop() {
 
+#ifdef WEB_INTERFACE
   server.handleClient();
+#endif
 
   if (Serial.available()) {
     char c = Serial.read();
     drive(c);
+    drive_arm(c);
+    arm_drive(c); // This is cursed....
   }
 }
 
@@ -137,6 +176,57 @@ void motor_init(MOTOR &p_motor, uint8_t direction_pin, uint8_t pwm_pin) {
 
   pinMode(direction_pin, OUTPUT);
   setSpeed(p_motor, 0);
+}
+
+/* Direction pin isn't really a direction choice, it just allows for pwm to appear negative for reverse */
+void motor_init(MOTOR_DIFF &p_motor, uint8_t direction_pin, uint8_t pwm_pin) {
+  p_motor.dir_A = direction_pin;
+  p_motor.pwm_B = pwm_pin;
+
+  p_motor.orientation = DEFAULT_ORIENTATION;
+
+  pinMode(direction_pin, OUTPUT);
+  setSpeed(p_motor, 0);
+}
+
+/* Values for speeds
+ * 0  , 0  => stop ==
+ * 0  , 1  => max  <<
+ * 1  , 0  => max  >>
+ * 0.5, 1  => 50%  <
+ * 0.5, 0  => 50%  >
+ * .25, 1  => 75%  <
+ * .75, 0  => 75%  >
+ * 
+ */
+int setSpeed(MOTOR_DIFF &p_motor, int velocity) {
+  if(velocity >  PWMMAX){
+    velocity = PWMMAX;
+  }
+  if(velocity < -(PWMMAX)){
+    velocity = -(PWMMAX);
+  }
+  p_motor.velocity = velocity;
+  if(p_motor.orientation){
+    velocity = velocity * -1;
+  }
+  if(velocity == 0){
+    p_motor.velocity = velocity;
+    digitalWrite(p_motor.dir_A, LOW);
+    analogWrite(p_motor.pwm_B, 0);
+    return 0;
+  }
+
+  if(velocity > 0){
+    int speed = map(velocity, 0, PWMMAX, 0,PWMMAX);
+    digitalWrite(p_motor.dir_A, LOW);
+    analogWrite(p_motor.pwm_B, speed);
+  } else {
+    int speed = map(velocity, -PWMMAX, 0, 0,PWMMAX);
+    digitalWrite(p_motor.dir_A, HIGH);
+    analogWrite(p_motor.pwm_B, speed);
+  }
+  return 0;
 }
 
 int setSpeed(MOTOR &p_motor, int velocity) {
@@ -204,3 +294,38 @@ void drive(char c){
     Serial.printf("Controls: %c%c%c%c\n", FORWARD, LEFT, BACKWARD, RIGHT);
   }
 }
+
+
+void drive_arm(char c){
+
+  if (CLOCKWISE == c) {
+    Serial.println("Clockwise");
+    setSpeed(motor_base,  (MTRINC*3/4));
+  } else if (A_CLOCKWISE == c) {
+    Serial.println("Anti-Clockwise");
+    setSpeed(motor_base, - (MTRINC*3/4));
+  } else {
+    if (motor_base.velocity > 0){
+      setSpeed(motor_base, motor_base.velocity*3/4);
+    } else if (motor_base.velocity < 0) {
+      setSpeed(motor_base, motor_base.velocity*3/4);
+    }
+  }
+
+}
+
+/*
+if (CLW == c) {
+  Serial.println("Clockwise");
+  setSpeed(motor_base, motor_base.velocity + MTRINC);
+} else if (ACLW == c) {
+  Serial.println("Anti-Clockwise");
+  setSpeed(motor_base, motor_base.velocity - MTRINC);
+} else {
+    if (motor_base.velocity > 0){
+      setSpeed(motor_base, motor_base.velocity - MTRINC);
+    } else if (motor_base.velocity < 0) {
+      setSpeed(motor_base, motor_base.velocity + MTRINC);
+    }
+  }
+*/
